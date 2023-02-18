fcb10dc6872c34c14c7e4266429c04d41b4090a9
Georgiy Sagitov <georgmc@yandex.ru>
2023-02-19 02:53:39 +0300

    MultiCurrent fix.

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import okio.Utf8
 import ru.netology.nmedia.dto.Post
 import java.io.IOException
 import java.util.concurrent.TimeUnit
@@ -52,8 +53,38 @@ class PostRepositoryImpl : PostRepository {
 
     }
 
+    override fun avatarLoad(post: Post, callback: PostRepository.Callback<Post>) {
 
-    override fun likeById(post: Post, callback: PostRepository.Callback<Post>){
+
+        val request: Request = Request.Builder()
+
+            .url("${BASE_URL}/api/slow/posts/${post.id}")
+
+            .build()
+
+        return client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    callback.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (!response.isSuccessful) {
+                        callback.onError(Exception(response.message))
+                        return
+                    }
+                    val data: Post = response.body?.string()
+                        .let { gson.fromJson(it, Post::class.java) }
+
+
+
+                    callback.onSuccess(data)
+                }
+            })
+
+    }
+
+    override fun likeById(post: Post, callback: PostRepository.Callback<Post>) {
         val likedByMeValue = post.likedByMe
 
         val answer: Request = Request.Builder().let {
@@ -68,12 +99,13 @@ class PostRepositoryImpl : PostRepository {
                 override fun onFailure(call: Call, e: IOException) {
                     callback.onError(e)
                 }
+
                 override fun onResponse(call: Call, response: Response) {
                     if (!response.isSuccessful) {
                         callback.onError(Exception(response.message))
                         return
                     }
-                    val data:Post= response.body?.string()
+                    val data: Post = response.body?.string()
                         .let { gson.fromJson(it, Post::class.java) }
 
 //                    data ?: run {
@@ -124,7 +156,7 @@ class PostRepositoryImpl : PostRepository {
 
     }
 
-    override fun removeById(id: Long,callback: PostRepository.Callback<Unit>) {
+    override fun removeById(id: Long, callback: PostRepository.Callback<Unit>) {
         val request: Request = Request.Builder()
             .delete()
             .url("${BASE_URL}/api/slow/posts/$id")
